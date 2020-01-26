2d15a3b28e548cdf4e028247c5fe8f82bc54ccf1
sumi <u22696@gmail.com>
2020-01-26 23:10:08 +0900

    modify gulpfile for v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,106 @@
 const gulp = require("gulp");
+const pug = require("gulp-pug");
 const sass = require("gulp-sass");
 const plumber = require("gulp-plumber");
-const browserSync = require("browser-sync");
 const notify = require("gulp-notify");
-const pug = require("gulp-pug");
+const uglify = require("gulp-uglify");
+const cssmin = require("gulp-cssmin");
+const browserSync = require("browser-sync");
+const del = require("del");
 
-gulp.task("default", ["browser-sync", "sass", "pug", "other", "watch"]);
+const paths =
+  { src: "src"
+  , dst: "docs"
+  };
 
-gulp.task("watch", () => {
-  gulp.watch(["src/**"], () => {
-    gulp.start(["sass", "pug", "other"]);
-  });
-});
-
-gulp.task("browser-sync", () => {
+gulp.task("browser-sync", done => {
   browserSync({
-    server: {
-      baseDir: "docs"
-    }
+    server: {baseDir: paths.dst}
   });
-  gulp.watch("src/**/*.*", ["reload"]);
+  gulp.watch(`${paths.src}/**/*.*`, gulp.task("reload"));
+  done();
 });
 
-gulp.task("sass", () => {
-  gulp.src("src/**/*.sass")
+gulp.task('sass', done => {
+  gulp.src([`${paths.src}/**/*.sass`])
     .pipe(plumber({
       errorHandler: notify.onError("Error: <%= error.message %>")
     }))
     .pipe(sass())
-    .pipe(gulp.dest("docs"))
-    .pipe(browserSync.stream())
+    .pipe(cssmin())
+    .pipe(gulp.dest(paths.dst));
+  done();
 });
 
-gulp.task("pug", () => {
-  const option =
-    { pretty: true
-    , basedir: "src"
-    };
-  gulp.src("src/**/*.pug")
+gulp.task("pug", done => {
+  gulp.src(`${paths.src}/**/*.pug`)
     .pipe(plumber({
       errorHandler: notify.onError("Error: <%= error.message %>")
     }))
-    .pipe(pug(option))
-    .pipe(gulp.dest("docs"))
+    .pipe(pug(
+      { pretty: true
+      , basedir: paths.src
+      }
+    ))
+    .pipe(gulp.dest(paths.dst))
+  done();
 });
 
-gulp.task("other", () => {
-  gulp.src(["src/**/*", "!src/**/*.pug", "!src/**/*.sass"])
+gulp.task('pug', done => {
+  gulp.src(
+    [ `${paths.src}/**/*.pug`
+    , `!${paths.src}/**/_*.pug`
+    ]
+  )
     .pipe(plumber({
       errorHandler: notify.onError("Error: <%= error.message %>")
     }))
-    .pipe(gulp.dest("docs"))
-    .pipe(browserSync.stream())
+    .pipe(pug({pretty: true, basedir: paths.src}))
+    .pipe(gulp.dest(paths.dst));
+  done();
 });
 
-gulp.task("reload", () => {
-  browserSync.reload();
+gulp.task('js', done => {
+  gulp.src(`${paths.src}/**/*.js`)
+    //.pipe(uglify())
+    .pipe(gulp.dest(paths.dst));
+  done();
 });
+
+gulp.task("other", done => {
+  gulp.src(
+      [ `${paths.src}/**/*`
+      , `!${paths.src}/**/*.pug`
+      , `!${paths.src}/**/*.sass`
+      , `!${paths.src}/**/*.js`
+      ]
+    )
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
+    .pipe(gulp.dest(paths.dst))
+    .pipe(browserSync.stream())
+  done();
+});
+
+gulp.task('watch', done => {
+  gulp.watch(`${paths.src}/**/*.sass`).on('change', gulp.series('sass', browserSync.reload));
+  gulp.watch(`${paths.src}/**/*.pug`).on('change', gulp.series('pug', browserSync.reload));
+  gulp.watch(`${paths.src}/**/*.js`).on('change', gulp.series('js', browserSync.reload));
+  gulp.watch(`${paths.src}/**/*`).on('change', gulp.series('other', browserSync.reload));
+  done();
+});
+
+gulp.task('reload', done => {
+  browserSync({server: {baseDir: paths.dst}});
+  done();
+});
+
+gulp.task('clean', done => {
+  del.sync(`${paths.dst}/**`, `!${paths.dst}`);
+  done();
+});
+
+gulp.task('default',
+  gulp.parallel('pug', 'sass', 'js', 'other', 'watch', 'reload', done => {done();})
+);
